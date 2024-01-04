3d986fa8b6e6a21b53f637a81d3f137cbdbe383a
viktechsolutions <viktechsolutions@yandex.ru>
2024-01-04 10:19:25 +0300

    initial commit

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactLoading from "react-loading";
+import "./Spinner.scss";
+
+const Spinner = ({type, color} : {type: any, color: string}) => {
+
+    return (
+       <ReactLoading className="spinner" type={type} color={color} width={'50%'}/>
+    );
+}
+
+export default Spinner;
